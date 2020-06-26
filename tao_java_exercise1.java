aa675e2776009a9795951090531905b942a25783
Tao Tao <taotao1@msu.edu>
2020-06-25 21:56:32 -0400

    adding exercise1

---
+++ tao_java_exercise1.java
@@ -0,0 +1,14 @@
+package com.amazing.shoppingcart;
+//tao950823@gmail.com java exercise1
+public class tao_java_exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pounds=100;
+		double kg=0.454*pounds;
+		System.out.println(kg);
+
+		
+	}
+
+}
